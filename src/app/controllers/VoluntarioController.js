9b660d7440478734fa2fb7a78c0d3d9e95ca21d5
alesalg <allesalg@gmail.com>
2022-02-20 18:22:50 -0300

    metodo listar voluntario

--- src/app/controllers/VoluntarioController.js
+++ src/app/controllers/VoluntarioController.js
@@ -10,11 +10,35 @@ class VoluntarioController {
     });
 
     if(!name || !voluntario.contato.telefone || !voluntario.contato.email) {
-      return res.status(402).json({message: 'voluntario-parametros-vazios'})
+      return res.status(402).json({message: 'Parametros vazios'})
     }
 
     await voluntario.save();
-    return res.status(201).json({message: `voluntario-${voluntario.name}-cadastrado}`});
+    return res.status(201).json({message: `Voluntário ${voluntario.name} cadastrado`});
+  }
+  
+  static async index(req, res){
+    const voluntarios = await Voluntario.find()
+
+    if(voluntarios.length === 0 ){
+      res.status(402).json({message: 'Lista de Voluntarios Vazia'})
+      return
+    }
+
+    res.status(200).json(voluntarios)
+  }
+
+  static async show(req, res){
+    try {
+      const voluntario = await Voluntario.findById(req.params.id);
+      if(!voluntario) {
+        return res.status(404).json({message: "Voluntario não encontrado"})
+      } else {
+        return res.status(200).json(voluntario);
+      }
+    } catch (err) {
+      return res.status(400).json({ message: "ID informado não é uma string de 12 bytes ou uma string de 24 caracteres hex" });
+    }
   }
 }
 
