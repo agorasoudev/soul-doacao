627ee06d86fb6e279653dcdd6299849be3cd0572
alesalg <allesalg@gmail.com>
2022-02-21 16:01:18 -0300

    deletar usuario por id

--- src/app/controllers/VoluntarioController.js
+++ src/app/controllers/VoluntarioController.js
@@ -40,6 +40,7 @@ class VoluntarioController {
       return res.status(400).json({ message: "ID informado não é uma string de 12 bytes ou uma string de 24 caracteres hex" });
     }
   }
+
   static async update(req, res){
     try {
       const voluntario = await Voluntario.findByIdAndUpdate(req.params.id, req.body, { new: true })
@@ -50,6 +51,22 @@ class VoluntarioController {
       }
     } catch(err) {
       return res.status(400).json({message: "Erro ao atualizar voluntário"})
+    }
+  }
+
+  static async destroy(req, res){
+    const { id } = req.body;
+    
+    if(!id ) {
+      return res.status(400).json({message: "É necessário informar o ID do voluntário"})
+    }
+    if(id){
+      const voluntario = await Voluntario.findByIdAndDelete(id);
+      if(voluntario) {
+        return res.status(200).json({message: "Voluntario deletado com sucesso"})
+      } else {
+        return res.status(404).json({message: "Voluntario não encontrado"})
+      }
     }
   }
 }
